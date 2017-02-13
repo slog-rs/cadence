2f3a9ac73b7102d863d67c507ccb024a72ca5842
Nick Pillitteri <nick@tshlabs.org>
2017-02-13 09:04:22 -0500

    Skeletons for Drain and Error impls

--- src/lib.rs
+++ src/lib.rs
@@ -5,16 +5,66 @@
 extern crate cadence;
 extern crate slog;
 
+use std::error;
+use std::fmt;
 use std::sync::Arc;
-use cadence::MetricClient;
+use cadence::{MetricClient, MetricError};
+use slog::{Drain, Record, OwnedKeyValueList};
+
 
 ///
 ///
 ///
+#[derive(Clone)]
 pub struct CadenceDrain {
     client: Arc<MetricClient + Sync + Send>,
 }
 
-pub struct DrainBuilder<'a> {
-    prefix: &'a str,
+impl CadenceDrain {
+    ///
+    ///
+    ///
+    pub fn new<T>(client: T) -> CadenceDrain where T: MetricClient + Sync + Send + 'static {
+        CadenceDrain { client: Arc::new(client) }
+    }
+}
+
+
+impl Drain for CadenceDrain {
+    type Error = ::Error;
+
+    fn log(&self, info: &Record, _: &OwnedKeyValueList) -> Result<(), Error> {
+        Ok(())
+    }
+}
+
+
+///
+///
+///
+#[derive(Debug)]
+pub enum Error {
+    ClientError(MetricError),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match *self {
+            Error::ClientError(ref e) => e.fmt(f),
+        }
+    }
+}
+
+impl error::Error for Error {
+    fn cause(&self) -> Option<&error::Error> {
+        match *self {
+            Error::ClientError(ref e) => Some(e),
+        }
+    }
+
+    fn description(&self) -> &str {
+        match *self {
+            Error::ClientError(ref e) => e.description()
+        }
+    }
 }
